e4953ba7543976e1d600efceb2094b5ca09e2b1c
jacquescarlier <jacques.carlier@gmail.com>
2025-01-13 15:33:15 +0100

    change code for display pdf

--- src/containers/curiculumModal/CvPdf.jsx
+++ src/containers/curiculumModal/CvPdf.jsx
@@ -1,25 +1,30 @@
-import * as React from 'react';
-import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-
+import * as React from "react";
 
 const Curiculum = () => {
-    const handlePrint = () => {
-        window.print();
-      };
-    window.scrollTo(0, 0)
-    return (
-        <div className=' modalContainer'>
-            <button className='imprimerPdf' onClick={handlePrint}>Imprimer</button>
-            <div className='retour'>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    <div className="cvpdf">
-                        <Viewer fileUrl="./cv2024-1.pdf" defaultScale={SpecialZoomLevel.PageFit} />
-                    </div>
-                </Worker>
-            </div>
-        </div>
-    );
+  const handlePrint = () => {
+    window.print();
+  };
+  window.scrollTo(0, 0);
+  return (
+    <div className=" modalContainer">
+      <button className="imprimerPdf" onClick={handlePrint}>
+        Imprimer
+      </button>
+      <div className="retour">
+        <object
+          data="./cv2024-1.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <p>
+            Alternative text - include a link{" "}
+            <a href="./cv2024-1.pdf">to the PDF!</a>
+          </p>
+        </object>
+      </div>
+    </div>
+  );
 };
 
 export default Curiculum;
