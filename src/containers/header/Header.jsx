94db3a70270650ef190eed7820278fb1a6185073
jacquescarlier <jacques.carlier@gmail.com>
2025-01-13 14:59:52 +0100

    1st

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="header" id="top">
+      <span className="logoRotate">Copyright JCR</span>
+      <ul className="navigation">
+        <li className="liNavigation"><a href="#about">About</a></li>
+        <li className="liNavigation"><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+export default Header;
